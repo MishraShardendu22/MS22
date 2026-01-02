ed9d8190027c30b0d18e5a5bf76e26d10e0db002
Shardendu Mishra <shardendumishrabrave@gmail.com>
2026-01-03 03:12:00 +0530

    fix: format BackendURL assignment for consistency

--- src/static/data.ts
+++ src/static/data.ts
@@ -1,4 +1,4 @@
 export const BaseURL: string = process.env.NEXT_PUBLIC_BASE_URL as string;
-export const BackendURL: string = 
-  process.env.NEXT_PUBLIC_BACKEND_URL || 
+export const BackendURL: string =
+  process.env.NEXT_PUBLIC_BACKEND_URL ||
   "https://portfolio-backend-2iw4.onrender.com";
