da3e534bce7b140174ee5d0776978b054e0d29e2
Shardendu Mishra <shardendumishrabrave@gmail.com>
2026-02-20 00:39:50 +0530

    fix: made UI imrpovements minor
    Signed-off-by: Shardendu Mishra <shardendumishrabrave@gmail.com>

--- src/component/Hero/ImageContainer.tsx
+++ src/component/Hero/ImageContainer.tsx
@@ -13,7 +13,7 @@ export const ImageContainer = () => {
     >
       <div className="glow-effect" />
 
-      <div className="image-container group relative w-full aspect-3/4 max-w-[280px] sm:max-w-[320px] md:max-w-[400px] lg:max-w-[450px] xl:max-w-[500px] rounded-2xl overflow-hidden cursor-pointer mx-auto shadow-2xl transition-all duration-500 ease-out hover:-translate-y-2 hover:rotate-y-2">
+      <div className="image-container group relative w-full aspect-3/4 max-w-[280px] sm:max-w-[320px] md:max-w-[400px] lg:max-w-[450px] xl:max-w-[500px] rounded-2xl overflow-hidden cursor-pointer mx-auto shadow-2xl transition-all duration-500 ">
         <Image
           src={CDN_ICON_AVIF}
           alt="Shardendu Mishra - Software Developer and Engineer portrait"
